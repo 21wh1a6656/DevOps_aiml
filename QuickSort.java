9a1045fd46aadc8e10d85fe9c08be987ca437b66
21wh1a6625 <21wh1a6656@bvrithyderabad.edu.in>
2024-06-04 12:17:13 +0530

    firstchange

---
+++ QuickSort.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.event.*;
+import java.util.scanner;
+import java.util.swing;
+public class QuickSort{
+    public void static main(string args[]){
+        
+    }
+
+    public void quicksortalgo(lower,upper){
+        int lb = lower;
+        int ub = upper;
+        lb = (lower + (upper - lower)/2);
+        while(lb <= ub){
+            
+            lb++;
+        }
+        while(lb > ub){
+            
+            ub--;
+        }
+        if(lb <= ub){
+            
+        }
+    }
+}
